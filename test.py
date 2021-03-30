bf90515723438940470d911a47b56ef3bfc50a17
Zayonx-0 <61589082+Zayonx-0@users.noreply.github.com>
2021-03-30 13:54:35 +0200

    Presque fini
    J'ai fini la traduction du son en . et - et  .

--- test.py
+++ test.py
@@ -10,7 +10,6 @@ realdata = []
 
 for i in data:
     realdata += [i]
-    print(i)
 
 def determineBeepTime(realdata):
     SupposedSilence = 0
@@ -25,7 +24,6 @@ def determineBeepTime(realdata):
         if abs(realdata[index]) > 500:
             if SupposedFinalSilence > SupposedSilence and SupposedSilence > 10:
                 SupposedFinalSilence = SupposedSilence
-                print(SupposedFinalSilence)
             SupposedSilence = 0
             index += 1
             continue
@@ -40,15 +38,80 @@ def FinalSilenceTime(realdata):
         if abs(Analyze[i]) > 500:
             return len(realdata) - i
 
-BeepTime = determineBeepTime(realdata)
 
+def CheckLastTen(index, realdata):
+    current = abs(realdata[index])
+    if current < 500:
+        for i in range(0,10):
+            if realdata[index-i] < 500:
+                continue
+            else:
+                return False # Means that last 10 are NOT the same
+    else:
+        for i in range(0,10):
+            if realdata[index-i] > 500:
+                continue
+            else:
+                return False
 
-# Si c'est inferieur à 500 pendant au moins 10 fois, c'est un silence.
+    return True # last 10 ARE the same ! It means that there are nothing worng here. (Like bit of err)
+
+BeepTime = determineBeepTime(realdata)# 676 dans cet exemple. 
+
+# But : reconnaitre un ., un - et reconnaitre 5 . d'espace, qui signale une autre lettre.
+# Creer un Array de type : ['.','-','.','-',''/.....]
+# Maybe time everything ? Like there is sound for 500 sample rate ?
+# /..... = Espacement de deux lettres. 
 
 def Analyzer(BeepTime, realdata):
-    index = 0
-    row = 0
+    index = 10
     current = 0
-
+    Array = [0]
+    ArrayIndex = 0
+    last = ['']
     while index < len(realdata):
-        realdata[index]
+        # 500 Is the treshold, over 500 and we have a noise; under and we have silence
+        if abs(realdata[index]) < 400 and abs(realdata[index -2]) < 400 and abs(realdata[index - 1]) < 400: #Maybe try to do something according to the samplerate
+            if last[ArrayIndex] == 'Silence':
+                Array[ArrayIndex] = Array[ArrayIndex] + 1
+            else:
+                ArrayIndex +=1
+                last.append('Silence')
+                Array.append(1)
+        else:
+            if last[ArrayIndex] == 'Noise':
+                Array[ArrayIndex] = Array[ArrayIndex] + 1
+            else:
+                ArrayIndex +=1
+                last.append('Noise')
+                Array.append(1)
+        index += 1
+    return Array, last
+
+result = Analyzer(BeepTime, realdata)
+NoiseTimes = result[0]
+NoiseNature = result[1]
+
+def translator(NoiseTimes, NoiseNature):
+    translated = []
+    for i in range(len(NoiseNature)):
+        if NoiseNature[i] == 'Silence':
+            if NoiseTimes[i] > BeepTime - 50 and NoiseTimes[i] < BeepTime + 50:
+                translated.append('')
+            elif NoiseTimes[i] > BeepTime * 5:
+                translated.append(' ')
+        else:
+            if NoiseTimes[i] > BeepTime - 50 and NoiseTimes[i] < BeepTime + 50:
+                translated.append('.')
+            elif NoiseTimes[i] > BeepTime * 2:
+                translated.append('-')
+    return translated
+
+toprint = ''
+
+done = translator(NoiseTimes, NoiseNature)
+
+for i in range(len(done)):
+    toprint += done[i]
+
+print(toprint)
